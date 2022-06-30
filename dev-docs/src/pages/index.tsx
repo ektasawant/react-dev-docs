7e5e8e38dfa9f97442a697ceef5dc9e66ab6e2d9
Ekta Ghag <ektasawant99@gmail.com>
2022-06-30 13:52:25 +0530

    revert changes

--- dev-docs/src/pages/index.tsx
+++ dev-docs/src/pages/index.tsx
@@ -1,1 +1,2 @@
 export * from './componentPreview';
+export * from './home';
--- dev-docs/src/router/routes.tsx
+++ dev-docs/src/router/routes.tsx
@@ -1,7 +1,14 @@
-import { ComponentPreview } from '../pages';
+import { HomePage, ComponentPreview } from '../pages';
+import Home from '@mui/icons-material/Home';
 import LooksOne from '@mui/icons-material/LooksOne';
 
 export const PAGES = [
+    {
+        title: 'Home Page',
+        route: '',
+        component: HomePage,
+        icon: Home,
+    },
     {
         title: 'Component preview',
         route: 'component-preview',
